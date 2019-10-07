80ec5b9b297726221c164dfd337278b7cd4253cd
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2019-10-07 13:35:48 +0530

    flask api

---
+++ quotes.py
@@ -0,0 +1,26 @@
+def funny_quotes():
+    return [{
+        "author": "xyz",
+        "quote": "hdj"
+        },
+        {
+        "author": "xyz1",
+        "quote": "hdj1"  
+        },
+        {
+        "author": "xyz2",
+        "quote": "hdj2"  
+        },
+        {
+        "author": "xyz3",
+        "quote": "hdj3"  
+        },
+        {
+        "author": "xyz4",
+        "quote": "hdj4"  
+        },
+        {
+        "author": "xyz5",
+        "quote": "hdj5"  
+        }
+    ]
